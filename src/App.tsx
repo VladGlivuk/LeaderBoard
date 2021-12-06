a79e6f73375a765c568b996b3745a42f970b3042
Vladislav <Vladislav.Glivuk@fivesysdev.com>
2021-12-06 16:09:22 +0200

    add loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import LeaderBoardHeader from 'components/LeaderBoardHeader';
 import LeadersBoard from 'components/LeaderBoardTable';
+import Loader from 'components/shared/Loader/Loader';
 import { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { IStore } from 'store';
+import { Leader } from 'store/leaderBoardReducer/types';
 import styles from './App.module.scss';
 import './normalize.scss';
 
-const App: FC = () => (
-  <div className={styles.wrapper}>
-    <LeaderBoardHeader />
-    <LeadersBoard />
-  </div>
-);
+const App: FC = () => {
+  const users: Array<Array<Leader>> = useSelector((store: IStore) => store.leaderBoard.allUsers);
+
+  return (
+    <div className={styles.wrapper}>
+      {users.length ? <LeaderBoardHeader /> : <Loader className={styles.loader} />}
+      <LeadersBoard />
+    </div>
+  );
+};
 
 export default App;
